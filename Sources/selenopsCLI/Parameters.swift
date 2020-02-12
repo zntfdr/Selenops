4f524bff5ee9fdfca36f0575ac782a87eb80b325
Federico Zanetello <zntfdr@gmail.com>
2020-02-12 20:48:42 +0700

    add Parameters struct

---
+++ Sources/selenopsCLI/Parameters.swift
@@ -0,0 +1,14 @@
+//
+//  Parameters.swift
+//  
+//
+//  Created by Federico Zanetello on 2/12/20.
+//
+
+import Foundation
+
+struct Parameters {
+  let startUrl: URL
+  let wordToSearch: String
+  let maximumPagesToVisit: Int
+}
