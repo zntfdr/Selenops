081549410dbca3a8b6c6484d878f0123abd0d1b7
Federico Zanetello <zntfdr@gmail.com>
2020-02-12 20:54:33 +0700

    move the parsing to new class Parser

---
+++ Sources/selenopsCLI/Parser.swift
@@ -0,0 +1,59 @@
+//
+//  Parser.swift
+//  
+//
+//  Created by Federico Zanetello on 2/12/20.
+//
+
+import Foundation
+import TSCUtility
+
+/// Parses the input from the command line.
+///
+/// It also declares what are all the possible argument types.
+final class Parser {
+  private let arguments: [String]
+
+  public init(arguments: [String] = CommandLine.arguments) {
+    // We drop the first argument, which is the script path.
+    self.arguments = Array(arguments.dropFirst())
+  }
+
+  /// Returns the action requested by the user (if any).
+  public func parse() throws -> Parameters {
+    // Initializes and sets up the `ArgumentParser` instance.
+    let parser = ArgumentParser(
+      usage: "selenops",
+      overview: "Searches for the given word on the web"
+    )
+
+    let pageArgument: OptionArgument<Foundation.URL> = parser.add(
+      option: "--page",
+      shortName: "-p",
+      kind: Foundation.URL.self,
+      usage: "The starting URL"
+    )
+
+    let wordArgument: OptionArgument<String> = parser.add(
+      option: "--word",
+      shortName: "-w",
+      kind: String.self,
+      usage: "The word to look for"
+    )
+
+    let pageNumberArgument: OptionArgument<Int> = parser.add(
+      option: "--number",
+      shortName: "-n",
+      kind: Int.self,
+      usage: "The maximum number of pages to visit")
+
+    let parsedArguments: ArgumentParser.Result = try parser.parse(arguments)
+
+    let startUrl = parsedArguments.get(pageArgument) ?? URL(string: "https://developer.apple.com/swift/")!
+    let wordToSearch = parsedArguments.get(wordArgument) ?? "Swift"
+    let maximumPagesToVisit = parsedArguments.get(pageNumberArgument) ?? 10
+
+    return Parameters(startUrl: startUrl, wordToSearch: wordToSearch, maximumPagesToVisit: maximumPagesToVisit)
+  }
+}
+
--- Sources/selenopsCLI/main.swift
+++ Sources/selenopsCLI/main.swift
@@ -1,40 +1,8 @@
 import Foundation
 import TSCUtility
 
-let parser = ArgumentParser(
-  usage: "selenops",
-  overview: "Searches for the given word on the web"
-)
-
-let pageArgument: OptionArgument<Foundation.URL> = parser.add(
-  option: "--page",
-  shortName: "-p",
-  kind: Foundation.URL.self,
-  usage: "The starting URL"
-)
-
-let wordArgument: OptionArgument<String> = parser.add(
-  option: "--word",
-  shortName: "-w",
-  kind: String.self,
-  usage: "The word to look for"
-)
-
-let pageNumberArgument: OptionArgument<Int> = parser.add(
-  option: "--number",
-  shortName: "-n",
-  kind: Int.self,
-  usage: "The maximum number of pages to visit")
-
 do {
-  let arguments = Array(CommandLine.arguments.dropFirst())
-  let parsedArguments = try parser.parse(arguments)
-
-  let startUrl = parsedArguments.get(pageArgument) ?? URL(string: "https://developer.apple.com/swift/")!
-  let wordToSearch = parsedArguments.get(wordArgument) ?? "Swift"
-  let maximumPagesToVisit = parsedArguments.get(pageNumberArgument) ?? 10
-
-  print(startUrl, wordToSearch, maximumPagesToVisit)
+  let parameters = try Parser().parse()
 } catch ArgumentParserError.expectedValue(let value) {
     print("Missing value for argument \(value).")
 } catch ArgumentParserError.expectedArguments(let parser, let stringArray) {
