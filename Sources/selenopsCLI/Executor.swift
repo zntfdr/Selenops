0d7ffaa4e8d7103ee5d3f78768cb62cc69b08998
Federico Zanetello <zntfdr@gmail.com>
2020-02-12 20:57:10 +0700

    create and use Executor

---
+++ Sources/selenopsCLI/Executor.swift
@@ -0,0 +1,14 @@
+//
+//  Executor.swift
+//  
+//
+//  Created by Federico Zanetello on 2/12/20.
+//
+
+import Foundation
+
+final class Executor {
+  func run(parameters: Parameters) {
+    print(parameters)
+  }
+}
--- Sources/selenopsCLI/main.swift
+++ Sources/selenopsCLI/main.swift
@@ -3,6 +3,7 @@ import TSCUtility
 
 do {
   let parameters = try Parser().parse()
+  Executor().run(parameters: parameters)
 } catch ArgumentParserError.expectedValue(let value) {
     print("Missing value for argument \(value).")
 } catch ArgumentParserError.expectedArguments(let parser, let stringArray) {
