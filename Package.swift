af556bc8612e6bc70160d4f5d009a17ca0d26989
Federico Zanetello <zntfdr@gmail.com>
2020-02-15 10:37:52 +0700

    add platform requirement

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "selenops",
+    platforms: [
+      .macOS(.v10_15)
+    ],
     dependencies: [
       .package(url: "https://github.com/apple/swift-tools-support-core.git",
                from: "0.0.1")
