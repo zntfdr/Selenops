8962c987d4fc4840a57691ef9a0360ea6c1367c7
Federico Zanetello <zntfdr@gmail.com>
2020-02-25 18:42:55 +0700

    use ArgumentConversionError instead of new custom type

--- Sources/selenopsCLI/URL+ArgumentKind.swift
+++ Sources/selenopsCLI/URL+ArgumentKind.swift
@@ -10,15 +10,11 @@ import TSCUtility
 
 extension Foundation.URL: ArgumentKind {
 
-  enum InitializationError: Error {
-    case malformedURL
-  }
-
   public init(argument: String) throws {
     if let url = URL(string: argument) {
       self = url
     } else {
-      throw InitializationError.malformedURL
+      throw ArgumentConversionError.unknown(value: argument)
     }
   }
 
