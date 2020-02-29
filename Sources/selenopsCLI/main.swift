20cf1ae52b9750b04338fd1397614a05b88b6cfa
Federico Zanetello <zntfdr@gmail.com>
2020-02-29 08:14:43 +0700

    transfer parser information to Selenops ParsableCommand

--- Sources/selenopsCLI/main.swift
+++ Sources/selenopsCLI/main.swift
@@ -2,9 +2,28 @@ import Foundation
 import ArgumentParser
 
 struct Selenops: ParsableCommand {
-  @Option(name: .shortAndLong) var start: URL //startUrl
-  @Option(name: .shortAndLong) var word: String //wordToSearch
-  @Option(name: .shortAndLong, default: 10) var max: Int //maximumPagesToVisit
+  static var configuration = CommandConfiguration(
+    abstract: "Searches for the given word on the web."
+  )
+
+  @Option(
+    name: .shortAndLong,
+    help: "The starting page URL (must have http:// or https:// prefix)."
+  )
+  var start: URL //startUrl
+
+  @Option(
+    name: .shortAndLong,
+    help: "The word to look for."
+  )
+  var word: String //wordToSearch
+
+  @Option(
+    name: .shortAndLong,
+    default: 10,
+    help: "The maximum number of pages to visit."
+  )
+  var max: Int //maximumPagesToVisit
 
   func run() throws {
     print("✅ Searching for: \(word)")
